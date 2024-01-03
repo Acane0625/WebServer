58bd54098fe7f52f0fa6ff01486e958d6515715e
Acane0625 <Acane0625@outlook.com>
2024-01-03 17:12:54 +0800

    webserver

--- wmymain/src/main.cpp
+++ wmymain/src/main.cpp
@@ -1,6 +1,10 @@
-#include <webserver/buffer.h>
-#include <webserver/threadpool.h>
+#include<unistd.h>
+#include <webserver/webserver.h>
 
 int main() {
-     
+  webserver::WebServer server(
+        1316, 3, 60000, false,             /* 端口 ET模式 timeoutMs 优雅退出  */
+        3306, "root", "root", "webserver", /* Mysql配置 */
+        12, 6, true, 1, 1024);             /* 连接池数量 线程池数量 日志开关 日志等级 日志异步队列容量 */
+    server.start();
 }
